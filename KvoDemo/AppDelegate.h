5a6652fb8063172d9bccf833f0e47320670927fb
renjwjx <renjwjx@gmail.com>
2017-02-19 16:04:56 +0800

    Initial Commit

---
+++ KvoDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KvoDemo
+//
+//  Created by jinren on 19/02/2017.
+//  Copyright © 2017 jinren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ KvoDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  KvoDemo
+//
+//  Created by jinren on 19/02/2017.
+//  Copyright © 2017 jinren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
